78810c2e2a7b9619af8a5b705e2e751cf2711445
sharanveerk <sharanveerk@bootesnull.com>
2022-04-13 12:23:50 +0530

    implement jwt and make register api, now i am working on authentication middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const usersRouter = require("../api/routers/user.router")
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// router.get('/api',function(req,res,next){
+//   res.json({
+//     success:200,
+//     message:"success"
+//   });
+// });
 
+router.use("/api/create-user",usersRouter);
 module.exports = router;
